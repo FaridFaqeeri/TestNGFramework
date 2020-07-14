e45cb5b4a4f0a28f44dcb7edbe5d5c2b4b54fd74
FaridFaqeeri <FaridFaqeeri278@gmail.com>
2020-07-14 19:30:11 -0400

    second commit

---
+++ ClonecodeProject/src/main/java/ClonecodeProject/ClonecodeProject/App.java
@@ -0,0 +1,13 @@
+package ClonecodeProject.ClonecodeProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
